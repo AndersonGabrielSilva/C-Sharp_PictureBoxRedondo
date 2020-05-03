7f4eaf57475e7893166676104b8507814ecc1de5
andersongabsilva <Andersongabriel_maria@hotmail.com>
2020-05-03 18:32:21 -0300

    Adicionar arquivos de projeto.

---
+++ PictureBoxRedondo/frmPrincipal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureBoxRedondo
+{
+    public partial class frmPrincipal : Form
+    {
+        OpenFileDialog openFileDialog;
+        public frmPrincipal()
+        {
+            InitializeComponent();
+            openFileDialog = new OpenFileDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                roundPicture.ImageLocation = openFileDialog.FileName;
+                roundPicture.SizeMode = PictureBoxSizeMode.StretchImage;
+                roundPicture.Load();
+            }
+        }
+
+        private void Grafico_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
